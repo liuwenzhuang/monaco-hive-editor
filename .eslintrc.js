7ff922c5086e53ef230e340cb1740f3b0fc645af
刘文壮 <liuwenzhuang@corp.netease.com>
2021-08-17 16:28:08 +0800

    chore: project structure with lerna、yarn2、yarn workspaces

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports-ts': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+    'unused-imports/no-unused-vars-ts': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+  },
+}
